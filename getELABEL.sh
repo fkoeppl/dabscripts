e76405bd9d5a960eb6f1beab4b2a953674e01e3d
Florian Koeppl <f.koepplgmail.com>
2020-06-18 23:04:30 +0200

    Fixed and improved label detection

--- getELABEL.sh
+++ getELABEL.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-# Ensamble ID
-#etisnoop -n 50 -i temp.eti | grep 'Ensemble ID: 0x.' | sort | uniq | sed -e 's/^.*Ensemble ID: //g' | sed -e 's/ (Country.*$//g'
-EID=$(etisnoop -n 50 -i temp.eti | grep 'Ensemble ID: 0x.' | sort | uniq | sed -e 's/^.*Ensemble ID: 0x//g')
-#| sed -e 's/ (Country.*$//g')
+# Ensemble ID
+EID=$(etisnoop -n 50 -i $1 | grep 'Ensemble ID: 0x.' | sort | uniq | head -1 | sed -e 's/^.*Ensemble ID: 0x//g' | sed -e 's/[^a-zA-Z0-9\\.\\-]/_/g')
 echo -----------------
 echo Ensemble ID: $EID
-echo .
+echo 
 
-# Ensamble Label
-ELABEL=$(etisnoop -n 50 -i temp.eti | grep -A 2 'Ensemble ID:' | grep 'Label: ' | sort | uniq | sed -e 's/^.*Label: "//g' | sed -e 's/".*$//g')
+# Ensemble Label
+ELABEL=$(etisnoop -n 50 -i $1 | grep -A 2 'Ensemble ID:' | grep 'Label: ' | sort | uniq | head -1  | sed -e 's/^.*Label: "//g' | sed -e 's/".*$//g'  | sed -e 's/[^a-zA-Z0-9\\.\\-]/_/g' )
 echo -----------------
 echo Ensemble Label: $ELABEL
-echo .
-
-#etisnoop -n 50 -i temp.eti | grep -A 2 'Ensemble ID:' | grep 'Label: ' | sort | uniq | sed -e 's/^.*Label: "//g' | sed -e 's/".*$//g'
-
-
+echo 
 
 
 
